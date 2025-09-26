c2c154b5b2e175aef8e5969ed5b3c1186ea8e1a6
heejoo11 <akhj55764586@gmail.com>
2025-09-26 14:32:53 +0900

    Update and rename 08P07 to 08P07_2.ino

--- 08P07_2.ino
+++ 08P07_2.ino
@@ -1,9 +1,9 @@
-
+// Arduino pin assignment
 #define PIN_LED  9
 #define PIN_TRIG 12   // sonar sensor TRIGGER
 #define PIN_ECHO 13   // sonar sensor ECHO
 
-
+// configurable parameters
 #define SND_VEL 346.0     // sound velocity at 24 celsius degree (unit: m/sec)
 #define INTERVAL 100       // sampling interval (unit: msec)
 #define PULSE_DURATION 10 // ultra-sound Pulse Duration (unit: usec)
